8ae332d48d42d78303e532afea5086a03965a443
Fernando Figueiredo <bookaneers@gmail.com>
2021-12-15 12:28:27 -0700

    Completed set up

---
+++ main.js
@@ -0,0 +1,21 @@
+const customersDisc = [
+  { id: 1, customer: 'Big Fish', disc: 50 },
+  { id: 2, customer: 'Medium Fish', disc: 30 },
+  { id: 3, customer: 'Little Fish', disc: 10 },
+  { id: 4, customer: 'Small Fish', disc: 0 },
+  { id: 5, customer: 'Bryan', disc: 44 }
+];
+
+const orders = [
+  { id: 1, partNumber: 'AA1', qty: 1, price: 3.20 },
+  { id: 2, partNumber: 'AA2', qty: 1, price: 3.20 },
+  { id: 3, partNumber: 'AA3', qty: 1, price: 3.20 },
+  { id: 4, partNumber: 'AA4', qty: 1, price: 3.20 },
+  { id: 5, partNumber: 'AA5', qty: 1, price: 3.20 },
+  { id: 1, partNumber: 'AA6', qty: 1, price: 3.20 },
+  { id: 2, partNumber: 'AA7', qty: 1, price: 3.20 },
+  { id: 3, partNumber: 'AA8', qty: 1, price: 3.20 },
+];
+
+console.log(customersDisc);
+console.log(orders);
