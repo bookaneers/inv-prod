5ff1975576decbe7e6bfa28b077d1dea17e06b30
Fernando Figueiredo <bookaneers@gmail.com>
2021-12-15 16:52:07 -0700

    Completed set-up

--- main.js
+++ main.js
@@ -19,3 +19,83 @@ const orders = [
 
 console.log(customersDisc);
 console.log(orders);
+
+const $customers = document.querySelector('#customers');
+const $orders = document.querySelector('#orders');
+const $report = document.querySelector('#report');
+const $customersForm = document.querySelector('#c-form');
+
+document.querySelector('#customers-view').addEventListener('click', (event)=> {
+
+  $customers.className = 'view';
+  $orders.className = 'view hidden';
+  $report.className = 'view hidden';
+  $customersForm.className = 'view hidden';
+
+  const $uList = document.querySelector('#cust-list');
+
+  for (let i = 0; i < customersDisc.length; i++) {
+
+    const $displayId = document.createElement('h3');
+    $displayId.textContent = customersDisc[i].id;
+    const $displayCustomer = document.createElement('h3');
+    $displayCustomer.textContent = customersDisc[i].customer;
+    const $displayDisc = document.createElement('h3');
+    $displayDisc.textContent = customersDisc[i].disc;
+
+    const $icon = document.createElement('img');
+    // $icon.setAttribute('class', 'pen');
+    $icon.setAttribute('src', 'images/redpen.png');
+    $icon.setAttribute('class', 'pen');
+    // DATA-ID
+    $icon.setAttribute('data-id', i);
+
+    $uList.appendChild($displayId);
+    $uList.appendChild($displayCustomer);
+    $uList.appendChild($displayDisc);
+    $uList.appendChild($icon);
+    console.log(i);
+  }
+
+});
+
+document.querySelector('#new-entry').addEventListener('click', (event) => {
+  $customers.className = 'view hidden';
+  $orders.className = 'view hidden';
+  $report.className = 'view hidden';
+  $customersForm.className = 'view';
+
+  const $contactForm = document.querySelector('#contact-form');
+
+  $contactForm.addEventListener('submit', function (event) {
+
+    event.preventDefault();
+    const entry = {
+      id: customersDisc.length + 1,
+      customer: $contactForm.elements['form-customer'].value,
+      discount: $contactForm.elements['form-disc'].value,
+    };
+
+    customersDisc.push(entry);
+    console.log(entry);
+    console.log(customersDisc);
+
+    document.getElementById('contact-form').reset();
+  });
+});
+
+document.querySelector('#orders-view').addEventListener('click', (event) => {
+  $customers.className = 'view hidden';
+  $orders.className = 'view';
+  $report.className = 'view hidden';
+  $customersForm.className = 'view hidden';
+
+});
+
+document.querySelector('#report-view').addEventListener('click', (event) => {
+  $customers.className = 'view hidden';
+  $orders.className = 'view hidden';
+  $report.className = 'view';
+  $customersForm.className = 'view hidden';
+
+});
